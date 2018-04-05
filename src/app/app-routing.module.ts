89c640c2317f213e8523cd743cab1edc54a8b82c
Rohit Naik <rohit@zedo.com>
2018-04-05 12:33:39 +0530

    dusra commit dusra nasha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-
+{path:'cricket',loadChildren:'app/cricket/cricket.module#CricketModule'},
 {path:'football',loadChildren:'app/football/football.module#FootballModule'}
 
 ];
--- src/app/cricket/cricket-routing.module.ts
+++ src/app/cricket/cricket-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeamsComponent } from './teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+ {path:'team',component:TeamsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
